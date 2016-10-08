5e3827c457a55e10f08f308c236606d4735f4950
Miguel Orellana <morellana@gmail.com>
2016-10-08 17:25:10 -0500

    reglas de validacion

---
+++ ProyectoCalendario/Scripts/CalendarioScript.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+
+
+
+    $('#tablaCalendario tr').click(function (event) {
+
+        $.ajax({
+            url: "/DetalleCalendarios/Filtra/" + $(this).attr('id'), success: function (result) {
+                $("#contenedorDetalle").html(result);
+                $("#contenedorPauta").html('');
+            }
+        });
+    });
+
+
+
+    $(document).on("click", ".sframe", function (e) {
+
+        $.ajax({
+            url: "/Pautas/Filtra/" + this.id, success: function (result) {
+                $("#contenedorPauta").html(result);
+            }
+        });
+    });
+
+    $('#FF').change(function (event) {
+
+        //("#FF").rules('add', { greaterThan: "#FI" });
+        if ($("#FF").val() < $("#FI").val()) {
+            alert('Fecha final menor que inicial.');
+            $("#FF").val('');
+        }
+    });
+    $('#FI').change(function (event) {
+        if ($("#FF").val() != "") {
+            //("#FF").rules('add', { greaterThan: "#FI" });
+            if ($("#FF").val() < $("#FI").val()) {
+                alert('FF' + $("#FF").val());
+                $("#FF").val('');
+            }
+        }
+    });
+
+
+});
+
